b028efdf7baec9e394899772dd212600cf366fa9
anbarasuacsp33 <anbarasuacsp@gmail.com>
2026-09-21 07:13:14 +0530

    Todyscommit_LocatorStrategy

--- tests/Search.spec.ts
+++ tests/Search.spec.ts
@@ -4,9 +4,19 @@ import {test} from '@playwright/test';
 test('Search', async ({page}) => {
 
     await page.goto('https://www.ebay.com/');
-    await page.locator('input.gh-search-input.gh-tb.ui-autocomplete-input').fill("iphone");
+    await page.locator("//div[@class='gh-search-input__wrap']").fill("iphone");
     await page.locator('button#gh-search-btn').click();
     await page.locator('h1#srp-results-heading').isVisible();
 
 });
+
+test('Search for Laptop', async ({page})=>{
+
+        await page.goto('https://www.ebay.com/');
+        await page.locator("//div[@class='gh-search-input__wrap']").fill("laptop");
+        await page.locator('button#gh-search-btn').click();
+        await page.locator('h1#srp-results-heading').isVisible();
+});
+
+
 
